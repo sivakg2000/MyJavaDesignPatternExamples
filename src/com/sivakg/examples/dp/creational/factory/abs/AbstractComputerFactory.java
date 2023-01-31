8fe5e2380ef2531e96ca8a760a7a167cfe6f79c1
Siva Karuppiah <sivakg2000@gmail.com>
2023-01-31 12:23:21 +0530

    Abstract Factory Design patter

---
+++ src/com/sivakg/examples/dp/creational/factory/abs/AbstractComputerFactory.java
@@ -0,0 +1,8 @@
+package com.sivakg.examples.dp.creational.factory.abs;
+
+import com.sivakg.examples.dp.creational.factory.Computer;
+
+public interface AbstractComputerFactory {
+
+	public abstract Computer createComputer();
+}

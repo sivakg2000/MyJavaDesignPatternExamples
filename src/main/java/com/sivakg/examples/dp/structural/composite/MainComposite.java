0bbdc718bed9381b28498164cfe71f984aae2273
Siva Karuppiah <sivakg2000@gmail.com>
2023-02-03 12:27:42 +0530

    Update MainComposite.java

--- src/main/java/com/sivakg/examples/dp/structural/composite/MainComposite.java
+++ src/main/java/com/sivakg/examples/dp/structural/composite/MainComposite.java
@@ -24,6 +24,14 @@ public class MainComposite {
 		sProvier.addService(new AdminService());
 		
 		sProvier.service("Logout Service");
+		
+		
+		sProvier.clearService();
+		
+		
+		sProvier.service("Update Service");
 	}
+	
+	
 
 }

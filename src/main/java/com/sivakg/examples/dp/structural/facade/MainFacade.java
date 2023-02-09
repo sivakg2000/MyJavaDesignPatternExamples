dcdbd5920e11fe476667a3218d16fa17f31ec5d8
Siva Karuppiah <sivakg2000@gmail.com>
2023-02-09 14:07:48 +0530

    Update MainFacade.java

--- src/main/java/com/sivakg/examples/dp/structural/facade/MainFacade.java
+++ src/main/java/com/sivakg/examples/dp/structural/facade/MainFacade.java
@@ -1,10 +1,30 @@
 package com.sivakg.examples.dp.structural.facade;
 
+import java.sql.Connection;
+
+import com.sivakg.examples.dp.structural.facade.impl.ExcelReportImpl;
+import com.sivakg.examples.dp.structural.facade.impl.HTMLReportImpl;
+import com.sivakg.examples.dp.structural.facade.impl.PDFReportImpl;
+
 public class MainFacade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Connection con=null;
+		String tableName="employee";
+		PDFReport pdfReport=new PDFReportImpl();
+		pdfReport.generatePDFReport(con, tableName);
+		
+		HTMLReport htmlReport=new HTMLReportImpl();
+		htmlReport.generateHTMLReport(con, tableName);
+		
+		
+		ExcelReport excelReport=new ExcelReportImpl();
+		excelReport.generateExcelReport(con, tableName);
+		
+		
+		
+		
 	}
 
 }

017201eebb6af56fbfb068a1a0e0bcb777def4f6
Siva Karuppiah <sivakg2000@gmail.com>
2023-02-10 10:37:15 +0530

    Update MainDecorator.java

--- src/main/java/com/sivakg/examples/dp/structural/decorator/MainDecorator.java
+++ src/main/java/com/sivakg/examples/dp/structural/decorator/MainDecorator.java
@@ -1,9 +1,20 @@
 package com.sivakg.examples.dp.structural.decorator;
 
+import com.sivakg.examples.dp.structural.decorator.impl.BasicBike;
+import com.sivakg.examples.dp.structural.decorator.impl.LuxuryBike;
+import com.sivakg.examples.dp.structural.decorator.impl.SportsBike;
+
 public class MainDecorator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { 
+		
+		Bike basicBike=new BasicBike();
+		basicBike.assembleBike();
+		Bike sportsBike=new SportsBike(basicBike);
+		sportsBike.assembleBike();
+		
+		Bike spLuxBike=new LuxuryBike(sportsBike);
+		spLuxBike.assembleBike();
 
 	}
 
